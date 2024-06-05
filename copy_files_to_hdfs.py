726f05d0c5c5abbc89bd4cb4dc4bd0d661ef16cd
Lashmanbala <lashmanbala@gmail.com>
2024-06-05 11:28:40 +0000

    unzipping the file and copying to hdfs

--- copy_files_to_hdfs.py
+++ copy_files_to_hdfs.py
@@ -12,7 +12,16 @@ path='data'
 cmd_template = 'hdfs dfs -put {src_dir} /user/`whoami`/data/{tgt_dir}'
 
 for item in glob.glob(f'{path}/*'):
-    cmd = cmd_template.format(src_dir=item, tgt_dir=os.path.split(item)[1])
-    print(f'copying {os.path.split(item)[1]} to hdfs')
-    #print(cmd)
-    subprocess.check_call(cmd, shell=True)
+    try:
+        print(f'unzipping {os.path.split(item)[1]}')
+        subprocess.check_call(f'gunzip {item}', shell=True)
+
+        file_name = os.path.split(item)[1][:-3]
+
+        cmd = cmd_template.format(src_dir=item[:-3], tgt_dir=file_name)
+
+        print(f'copying {os.path.split(item)[1]} to hdfs')
+        subprocess.check_call(cmd, shell=True)
+        
+    except subprocess.CalledProcessError as e:
+        print(f'failed to unzip {item} : {e}')
