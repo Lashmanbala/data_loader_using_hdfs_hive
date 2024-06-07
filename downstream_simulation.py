01ff840a2f364a93b4eac69442beced62707b778
Lashmanbala <lashmanbala@gmail.com>
2024-06-07 13:23:40 +0000

    automating the incoming data in a regular interval

---
+++ downstream_simulation.py
@@ -0,0 +1,31 @@
+import subprocess
+import glob
+import os
+import time
+
+
+path='data'
+
+cmd_template = 'hdfs dfs -put {src_dir} /user/`whoami`/nyse_data/{tgt_dir}'
+
+for item in glob.glob(f'{path}/*'):
+    try:
+        subprocess.check_call(
+                            'hdfs dfs -mkdir -p /user/`whoami`/nyse_data',
+                            shell=True
+                        )
+        
+        print(f'unzipping {os.path.split(item)[1]}')
+        subprocess.check_call(f'gunzip {item}', shell=True)
+
+        file_name = os.path.split(item)[1][:-3]
+
+        cmd = cmd_template.format(src_dir=item[:-3], tgt_dir=file_name)
+
+        print(f'copying {os.path.split(item)[1]} to hdfs')
+        subprocess.check_call(cmd, shell=True)
+
+        time.sleep(120)
+        
+    except subprocess.CalledProcessError as e:
+        print(f'failed to unzip {item} : {e}')
