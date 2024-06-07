59d594afee68275d5068dcc80d0e69076e8e127c
Lashmanbala <lashmanbala@gmail.com>
2024-06-07 06:10:32 +0000

    enabled logging to the standard location of hive log

--- wrapper.sh
+++ wrapper.sh
@@ -1,1 +1,1 @@
-hive -f /home/hadoop/data_loader_using_hdfs_hive/app.hql --verbose --hivevar user_name=`whoami`
+hive -f /home/hadoop/data_loader_using_hdfs_hive/app.hql --verbose --hivevar user_name=`whoami` >> /var/log/hive/user/hadoop/hive.log
